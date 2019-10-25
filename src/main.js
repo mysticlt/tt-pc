a1cfdb2b38201b2011687f56e00ba908f64f1c12
mysticlt <932721231@qq.com>
2019-10-25 16:42:57 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// isMainThread.js 职责是入口文件
+// 职责1：创建根实例
+// 职责2：依赖项目需要全局的资源

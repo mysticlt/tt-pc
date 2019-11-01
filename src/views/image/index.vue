e174ea3739738727daf79565c47e5ab625fe692b
mysticlt <932721231@qq.com>
2019-11-01 18:05:19 +0800

    素材管理 布局&&列表渲染 完成

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,15 +1,91 @@
 <template>
   <div class="container-image">
-      image
+    <el-card>
+      <div slot="header">
+        <my-bread>素材管理</my-bread>
+      </div>
+      <!-- 按钮 -->
+      <div class="btn_box">
+        <!-- 按钮单选框 -->
+        <el-radio-group v-model="reqParams.collect" size="small">
+          <el-radio-button label="true">全部</el-radio-button>
+          <el-radio-button label="false">收藏</el-radio-button>
+        </el-radio-group>
+        <!-- 绿色按钮 -->
+        <el-button style="float:right" type="success" size="small">添加素材</el-button>
+        <!-- 素材列表 -->
+        <div class="img_list">
+            <div class="img_item" v-for="item in images" :key="item.id">
+                <img :src="item.url">
+                <div class="footer">
+                    <span class="el-icon-star-off" :class="{red:item.is_collected}"></span>
+                    <span class="el-icon-delete"></span>
+                </div>
+            </div>
+        </div>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      reqParams: {
+        // false 全部 true 收藏
+        collect: true,
+        page: 1,
+        per_page: 10
+      },
+      // 素材列表
+      images: []
+    }
+  },
+  created () {
+    this.getImages()
+  },
+  methods: {
+    async getImages () {
+      const { data: { data } } = await this.$http.get('user/images', { params: this.reqParams })
+      this.images = data.results
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped>
+    .img_list{
+        padding-top: 20px;
+        .img_item{
+            width: 180px;
+            height: 180px;
+            position: relative;
+            border: 1px dashed #ddd;
+            display: inline-block;
+            margin-right: 50px;
+            margin-bottom: 20px;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+            .footer{
+                position: absolute;
+                left: 0;
+                bottom: 0;
+                height: 30px;
+                line-height: 30px;
+                background:rgba(0, 0, 0, 0.3);
+                width: 100%;
+                color: #fff;
+                text-align: center;
+                span{
+                    margin: 0 20px;
+                    &.red{
+                        color: red;
+                    }
+                }
+            }
+        }
+    }
+</style>>

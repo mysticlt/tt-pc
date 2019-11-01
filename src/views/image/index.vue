f7b542b871e4d022a6ff7c8e163aa428f3750f39
mysticlt <932721231@qq.com>
2019-11-01 20:07:49 +0800

    分页功能&&切换列表功能 完成

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -6,23 +6,30 @@
       </div>
       <!-- 按钮 -->
       <div class="btn_box">
-        <!-- 按钮单选框 -->
-        <el-radio-group v-model="reqParams.collect" size="small">
-          <el-radio-button label="true">全部</el-radio-button>
-          <el-radio-button label="false">收藏</el-radio-button>
+        <!-- 按钮单选框 label就是值 -->
+        <el-radio-group @change="toggleList" v-model="reqParams.collect" size="small">
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
         <!-- 绿色按钮 -->
         <el-button style="float:right" type="success" size="small">添加素材</el-button>
         <!-- 素材列表 -->
         <div class="img_list">
-            <div class="img_item" v-for="item in images" :key="item.id">
-                <img :src="item.url">
-                <div class="footer">
-                    <span class="el-icon-star-off" :class="{red:item.is_collected}"></span>
-                    <span class="el-icon-delete"></span>
-                </div>
+          <div class="img_item" v-for="item in images" :key="item.id">
+            <img :src="item.url" />
+            <div class="footer" v-if="reqParams.collect">
+              <span class="el-icon-star-off" :class="{red:item.is_collected}"></span>
+              <span class="el-icon-delete"></span>
             </div>
+          </div>
         </div>
+        <!-- 分页 -->
+        <el-pagination background layout="prev, pager, next"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+        ></el-pagination>
       </div>
     </el-card>
   </div>
@@ -34,58 +41,74 @@ export default {
     return {
       reqParams: {
         // false 全部 true 收藏
-        collect: true,
+        collect: false,
         page: 1,
         per_page: 10
       },
       // 素材列表
-      images: []
+      images: [],
+      // 总条数
+      total: 0
     }
   },
   created () {
     this.getImages()
   },
   methods: {
+    // 获取素材列表数据
     async getImages () {
-      const { data: { data } } = await this.$http.get('user/images', { params: this.reqParams })
+      const {
+        data: { data }
+      } = await this.$http.get('user/images', { params: this.reqParams })
       this.images = data.results
+      this.total = data.total_count
+    },
+    // 分页
+    pager (newPage) {
+      this.reqParams.page = newPage
+      this.getImages()
+    },
+    // 切换列表
+    toggleList () {
+      this.reqParams.page = 1
+      this.getImages()
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-    .img_list{
-        padding-top: 20px;
-        .img_item{
-            width: 180px;
-            height: 180px;
-            position: relative;
-            border: 1px dashed #ddd;
-            display: inline-block;
-            margin-right: 50px;
-            margin-bottom: 20px;
-            img{
-                width: 100%;
-                height: 100%;
-            }
-            .footer{
-                position: absolute;
-                left: 0;
-                bottom: 0;
-                height: 30px;
-                line-height: 30px;
-                background:rgba(0, 0, 0, 0.3);
-                width: 100%;
-                color: #fff;
-                text-align: center;
-                span{
-                    margin: 0 20px;
-                    &.red{
-                        color: red;
-                    }
-                }
-            }
-        }
+.img_list {
+  padding-top: 20px;
+  .img_item {
+    width: 180px;
+    height: 180px;
+    position: relative;
+    border: 1px dashed #ddd;
+    display: inline-block;
+    margin-right: 50px;
+    margin-bottom: 20px;
+    img {
+      width: 100%;
+      height: 100%;
     }
+    .footer {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      height: 30px;
+      line-height: 30px;
+      background: rgba(0, 0, 0, 0.3);
+      width: 100%;
+      color: #fff;
+      text-align: center;
+      span {
+        margin: 0 20px;
+        &.red {
+          color: red;
+        }
+      }
+    }
+  }
+}
 </style>>

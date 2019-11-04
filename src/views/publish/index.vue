984ee8d08d4f538c2eae5545f126aee8db729f0d
mysticlt <932721231@qq.com>
2019-11-04 19:14:17 +0800

    合并修改文章业务 完成

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -2,7 +2,7 @@
   <div class="container-publish">
     <el-card>
       <div slot="header">
-        <my-bread>发布文章</my-bread>
+        <my-bread>{{$route.query.id?'修改':'发布'}}文章</my-bread>
       </div>
       <!-- 表单 -->
       <el-form label-width="120px">
@@ -25,14 +25,19 @@
             <my-image v-model="articleForm.cover.images[0]"></my-image>
           </div>
           <!-- 多图 -->
-           <div v-if="articleForm.cover.type === 3">
+          <div v-if="articleForm.cover.type === 3">
             <my-image v-for="i in 3" :key="i" v-model="articleForm.cover.images[i-1]"></my-image>
           </div>
         </el-form-item>
         <el-form-item label="频道">
           <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item>
+        <el-form-item v-if="$route.query.id">
+          <el-button type="success" @click="update(false)">修改</el-button>
+          <el-button @click="update(true)">存入草稿</el-button>
+        </el-form-item>
+
+        <el-form-item v-else>
           <el-button type="primary" @click="create(false)">发表</el-button>
           <el-button @click="create(true)">存入草稿</el-button>
         </el-form-item>
@@ -78,17 +83,42 @@ export default {
       }
     }
   },
+  created () {
+    // 判断当前是否是编辑
+    const articleId = this.$route.query.id
+    if (articleId) {
+      // 获取当前文章的信息
+      this.getArticle(articleId)
+    }
+  },
   methods: {
+    // 获取文章信息
+    async getArticle (id) {
+      const {
+        data: { data }
+      } = await this.$http.get('articles/' + id)
+      // 表单填充 面包屑文字 按钮文字
+      this.articleForm = data
+    },
+    // 新建
     async create (draft) {
-    // 发表 存入草稿
+      // 发表 存入草稿
       await this.$http.post(`articles?draft=${draft}`, this.articleForm)
+      // 提示
+      this.$message.success(draft ? '存入草稿成功' : '发表成功')
+      // 去内容管理
+      this.$router.push('/article')
+    },
+    // 修改
+    async update (draft) {
+      // 修改 存入草稿
+      await this.$http.put(`articles/${this.articleForm.id}?draft=${draft}`, this.articleForm)
       // 提示
       this.$message.success(draft ? '存入草稿成功' : '发表成功')
       // 去内容管理
       this.$router.push('/article')
     }
   }
-
 }
 </script>
 

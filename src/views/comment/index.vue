e99f057f444cd387d1421f56a6c98b8d418bcb1c
mysticlt <932721231@qq.com>
2019-11-05 16:15:14 +0800

    评论管理-列表与分页渲染 完成

---
+++ src/views/comment/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container-comment">
+    <el-card>
+      <div slot="header">
+        <my-bread>评论管理</my-bread>
+      </div>
+      <!-- 表格 -->
+      <el-table :data="articles">
+        <el-table-column label="标题" width="400" prop="title"></el-table-column>
+        <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+        <el-table-column label="状态">
+            <template slot-scope="scope">
+                {{scope.row.comment_status?"正常":"关闭"}}
+            </template>
+        </el-table-column>
+        <el-table-column label="操作">
+            <template slot-scope="scope">
+                <el-button v-if="scope.row.comment_status" type="success" size="small">打开评论</el-button>
+                <el-button v-else type="danger" size="small">关闭评论</el-button>
+            </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+      ></el-pagination>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      articles: [],
+      reqParams: {
+        response_type: 'comment',
+        page: 1,
+        per_page: 20
+      },
+      total: 0
+    }
+  },
+  created () {
+    this.getArticles()
+  },
+  methods: {
+    async getArticles () {
+      // 注意: 获取文章列表，但是需要包含评论相关信息
+      const { data: { data } } = await this.$http.get('articles', { params: this.reqParams })
+      // 渲染
+      this.articles = data.results
+      // 总条数
+      this.total = data.total_count
+    },
+    pager (newPage) {
+      this.reqParams.page = newPage
+      this.getArticles()
+    }
+  }
+
+}
+</script>
+
+<style>
+</style>

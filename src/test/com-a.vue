11486504951fef50b0a0787f2ddb23fe59da7147
mysticlt <932721231@qq.com>
2019-11-06 15:29:12 +0800

    非父子组件传值

---
+++ src/test/com-a.vue
@@ -0,0 +1,26 @@
+ <!-- A组件接收数据，绑定事件 -->
+<template>
+  <div class="com-a">
+      COM-A ---{{str}}
+  </div>
+</template>
+
+<script>
+import eventBus from '@/eventBus'
+export default {
+  data () {
+    return {
+      str: ''
+    }
+  },
+  created () {
+    eventBus.$on('B2A', (data) => {
+      this.str = data
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/test/com-b.vue
@@ -0,0 +1,26 @@
+<!-- B组件传递数据，触发事件 -->
+<template>
+    <div class="com-b">
+        COM-B <button @click="fn">发送数据</button>
+    </div>
+</template>
+
+<script>
+import eventBus from '@/eventBus'
+export default {
+  data () {
+    return {
+      msg: '组件B的数据'
+    }
+  },
+  methods: {
+    fn () {
+      eventBus.$emit('B2A', this.msg)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
